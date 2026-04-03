6a6052d73b9b36110acdf4860374d4bba9fe7b94
satyampatil839-ship-it <satyampatil839@gmail.com>
2026-04-03 11:25:28 +0000

    commit

---
+++ iterate/seven.js
@@ -0,0 +1,11 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+//in above when we open a scope hence it should return  if there is no any {} then no need to return 
+//chaining of map and filter
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
