9f55517846f7e352567ea9d234afc1a1f569b264
satyampatil839-ship-it <satyampatil839@gmail.com>
2026-04-09 14:45:57 +0000

    commit

--- railwayreservation2/script.js
+++ railwayreservation2/script.js
@@ -51,6 +51,9 @@ function showSection(section){
     document.getElementById("search").style.display="none";
     document.getElementById("pnr").style.display="none";
     document.getElementById("bookings").style.display="none";
+document.getElementById("login").style.display="none";
+document.getElementById("register").style.display="none";
+document.getElementById("contact").style.display="none";
 
     document.getElementById(section).style.display="block";
 
@@ -136,4 +139,47 @@ function checkPNR(){
         document.getElementById("pnr-result").innerHTML =
         "PNR not found!";
     }
+}
+function loginUser(){
+
+    let username = document.getElementById("login-username").value;
+    let password = document.getElementById("login-password").value;
+
+    let storedUser = JSON.parse(localStorage.getItem("user"));
+
+    if(!storedUser){
+        document.getElementById("login-result").innerHTML =
+        "No user found. Please register first.";
+        return;
+    }
+
+    if(username === storedUser.username && password === storedUser.password){
+        document.getElementById("login-result").innerHTML =
+        "Login Successful!";
+    } else {
+        document.getElementById("login-result").innerHTML =
+        "Invalid Username or Password!";
+    }
+}
+function registerUser(){
+
+    let username = document.getElementById("reg-username").value;
+    let password = document.getElementById("reg-password").value;
+
+    if(username === "" || password === ""){
+        alert("Please fill all details!");
+        return;
+    }
+
+    let user = {
+        username: username,
+        password: password
+    };
+
+    // Save user in localStorage
+    localStorage.setItem("user", JSON.stringify(user));
+
+    document.getElementById("register-result").innerHTML =
+    "Registration Successful! Now login.";
+
 }
